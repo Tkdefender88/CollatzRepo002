eaa8b51d908bbeee5aa9ae60df6cc72d0fa37cb4
Tkdefender88 <tkdefender88@gmail.com>
2017-03-21 15:52:37 -0600

    More IO methods and removed repeated code

--- 3n+1/src/data/Collatz.java
+++ 3n+1/src/data/Collatz.java
@@ -9,74 +9,72 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.Writer;
+import java.util.ArrayList;
 
 class Collatz {
 	int int1, int2, count;
-	
+
 	BufferedReader br;
 	BufferedWriter bw;
-	
+
 	public static void main(String[] args) throws IOException {
 		Collatz c = new Collatz();
 		System.setIn(new FileInputStream(new File("input")));
 		c.setupIO(new InputStreamReader(System.in), new OutputStreamWriter(System.out));
+		c.countCollatz(c.getNumbers()[0], c.getNumbers()[1]);
 	}
-	
+
 	public void setupIO(Reader reader, Writer writer) {
 		br = new BufferedReader(reader);
 		bw = new BufferedWriter(writer);
-		
+
 	}
-	/*
-	@SuppressWarnings("unused")
-	public void handleIO() throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		while (true) {
-			String input = in.readLine();
-			String[] numbers = input.split(" ");
-			int1 = Integer.parseInt(numbers[0]);
-			int2 = Integer.parseInt(numbers[1]);
-			if(input == null){
-				break;
-			}
+
+	public Integer[] getNumbers() {
+		String line = getLine();
+		String[] tokens = line.split(" ");
+		ArrayList<Integer> lst = new ArrayList<>();
+		for (String s : tokens)
+			if (!s.trim().equals(""))
+				lst.add(Integer.parseInt(s));
+		return lst.toArray(new Integer[lst.size()]);
+	}
+
+	public String getLine() {
+		try {
+			return br.readLine();
+		} catch (IOException e) {
+			System.err.println("IOException: getLine()");
 		}
+
+		return "";
 	}
-	*/
+
+	/*
+	 * @SuppressWarnings("unused") public void handleIO() throws IOException {
+	 * BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	 * while (true) { String input = in.readLine(); String[] numbers =
+	 * input.split(" "); int1 = Integer.parseInt(numbers[0]); int2 =
+	 * Integer.parseInt(numbers[1]); if(input == null){ break; } } }
+	 */
 
 	public void countCollatz(int a, int b) {
-		if (a < b) {
-			for (int i = a; i <= b; i++) {
-				int count = 1;
-				int n = i;
-				while (n != 1) {
-					if (n % 2 == 0) {
-						n = n / 2;
-						count++;
-					} else {
-						n = (3 * n) + 1;
-						count++;
-					}
-				}
-				if (count > this.count) {
-					this.count = count;
+		int min, max;
+		if (a < b) {min = a;max = b;} else {min = b;max = a;}
+		for (int i = min; i <= max; i++) {
+			int count = 1;
+			int n = i;
+			while (n != 1) {
+				if (n % 2 == 0) {
+					n = n / 2;
+					count++;
+				} else {
+					n = (3 * n) + 1;
+					count++;
 				}
 			}
-		} else { // handles cases where second number is smaller than the first
-			for (int i = b; i <= a; i++) {
-				int count = 1;
-				int n = i;
-				while (n != 1) {
-					if (n % 2 == 0) {
-						n = n / 2;
-						count++;
-					} else {
-						n = (3 * n) + 1;
-						count++;
-					}
-				}
-				if (count > this.count) {
-					this.count = count;
-				}
+			if (count > this.count) {
+				this.count = count;
 			}
 		}
 	}

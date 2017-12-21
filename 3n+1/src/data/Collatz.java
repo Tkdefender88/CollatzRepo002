95856fe9fb21ad81132cb4e03fe56f8b880a8ca1
Tkdefender88 <tkdefender88@gmail.com>
2017-12-21 15:54:24 -0700

    refactored

--- 3n+1/src/data/Collatz.java
+++ 3n+1/src/data/Collatz.java
@@ -5,48 +5,47 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 class Collatz {
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	public int int1, int2;
 
-	public static void main(String[] args) throws IOException {
-		Collatz c = new Collatz();
-		c.run();
-		System.exit(0);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public void run() throws IOException {
-		String line;
-		while(!(line = br.readLine()).isEmpty()) {
-			handleInputOutput(line);
-		}
-	}
+        String line;
+        while (!(line = br.readLine()).isEmpty()) {
+            String[] numbers = line.split("\\s+");
+            int int1 = Integer.parseInt(numbers[0]);
+            int int2 = Integer.parseInt(numbers[1]);
+            int collatz = countCollatz(int1, int2);
+            System.out.println(int1 + " " + int2 + " " + collatz);
+        }
 
-	public void handleInputOutput(String line) {
-		String[] numbers = line.split("\\s+");
-		int1 = Integer.parseInt(numbers[0]);
-		int2 = Integer.parseInt(numbers[1]);
-		System.out.println(int1 + " " + int2 + " " + countCollatz(int1, int2));
-	}
+        System.exit(0);
+    }
 
-	public int countCollatz(int a, int b) {
-		int min, max, maxCount = 0;
-		if (a < b) {min = a;max = b;} else {min = b;max = a;}
-		for (int i = min; i <= max; i++) {
-			int count = 1;
-			int n = i;
-			while (n != 1) {
-				if (n % 2 == 0) {
-					n = n / 2;
-					count++;
-				} else {
-					n = (3 * n) + 1;
-					count++;
-				}
-			}
-			if (count > maxCount) {
-				maxCount = count;
-			}
-		}
-		return maxCount;
-	}
+    public static int countCollatz(int a, int b) {
+        int min, max, maxCount = 0;
+        if (a < b) {
+            min = a;
+            max = b;
+        } else {
+            min = b;
+            max = a;
+        }
+        for (int i = min; i <= max; i++) {
+            int count = 1;
+            int n = i;
+            while (n != 1) {
+                if (n % 2 == 0) {
+                    n = n / 2;
+                    count++;
+                } else {
+                    n = (3 * n) + 1;
+                    count++;
+                }
+            }
+            if (count > maxCount) {
+                maxCount = count;
+            }
+        }
+        return maxCount;
+    }
 }
